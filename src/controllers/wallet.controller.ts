f2292c524c22fa59d94c80d8fa28b0de2644d3c7
krtabares <krtabares@gmail.com>
2024-07-18 15:37:04 -0400

    waller by user

--- src/controllers/wallet.controller.ts
+++ src/controllers/wallet.controller.ts
@@ -37,3 +37,37 @@ export const getWallet = async (req: Request, res: Response): Promise<Response> 
         });
     }
 }
+
+export const getWalletByUser = async (req: Request, res: Response): Promise<Response> => {
+    const { id } = req.params;
+    if (id === null || id === undefined || !id || !ObjectId.isValid(id)) {
+        return res.status(httpCode[409].code).json({
+            data_send: "",
+            num_status: httpCode[409].code,
+            msg_status: 'El Id no es válido'
+        });
+    }
+    const data = await Wallet.findOne({userid:id})
+        .populate('userid', 'nombre')
+
+    try {
+        if (!data) {
+            return res.status(httpCode[200].code).json({
+                data_send: [],
+                num_status: httpCode[200].code,
+                msg_status: 'Itinerario no encontrado'
+            });
+        }
+        return res.status(httpCode[200].code).json({
+            data_send: data,
+            num_status: httpCode[200].code,
+            msg_status: 'Itinerario encontrada satisfactoriamente.'
+        });
+    } catch (error) {
+        return res.status(httpCode[500].code).json({
+            data_send: "",
+            num_status: httpCode[500].code,
+            msg_status: 'There was a problem with the server, try again later '
+        });
+    }
+}
--- src/routes/wallet.routes.ts
+++ src/routes/wallet.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { getWallet } from "../controllers/wallet.controller";
+import { getWallet, getWalletByUser } from "../controllers/wallet.controller";
 
 //import { validateCreate, validateUpdate } from '../validators/genero.validator';
 
 const router = Router();
 
-router.get('/show/:id', getWallet);
+router.get('/show/:id', getWalletByUser);
 
 
 export default router;
